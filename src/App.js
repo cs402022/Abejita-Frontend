4924494dc3ebac5051add203f7d47eb22a3ed9e8
cs402022 <90572548+cs402022@users.noreply.github.com>
2025-03-09 14:02:50 -0500

    Corregir Switch por Routes en react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Cambia Switch por Routes
 import Invitation from './components/Invitation';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/:nombre" component={Invitation} />
-      </Switch>
+      <Routes>
+        <Route path="/:nombre" element={<Invitation />} /> {/* Sintaxis actualizada con element */}
+      </Routes>
     </Router>
   );
 }
