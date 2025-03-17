b657449acae41b931a33b3e67417e46ecf1fab3d
cs402022 <90572548+cs402022@users.noreply.github.com>
2025-03-16 22:30:10 -0500

    modificacion2 whatspp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Cambia Switch por Routes
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importar Routes y Route
+import { HelmetProvider } from 'react-helmet-async'; // Importar HelmetProvider
 import Invitation from './components/Invitation';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/:nombre" element={<Invitation />} /> {/* Sintaxis actualizada con element */}
-      </Routes>
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <Routes>
+          <Route path="/:nombre" element={<Invitation />} /> {/* Ruta dinámica para el nombre */}
+        </Routes>
+      </Router>
+    </HelmetProvider>
   );
 }
 
